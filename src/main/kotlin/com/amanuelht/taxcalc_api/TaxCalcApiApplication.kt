34301a21cc37b14e0e14fe471172f219c188b783
amanuelHT <aman3hda@gmail.com>
2026-02-05 15:53:49 +0100

    Initial Spring Boot project setup

---
+++ src/main/kotlin/com/amanuelht/taxcalc_api/TaxCalcApiApplication.kt
@@ -0,0 +1,11 @@
+package com.amanuelht.taxcalc_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TaxCalcApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<TaxCalcApiApplication>(*args)
+}
